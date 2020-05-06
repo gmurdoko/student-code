8eeb0e08ff6e33ab32043a680456ec757373a1e9
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-06 11:33:33 +0700

    pertemuan 2 done

--- php-dasar/pertemuan2/index.php
+++ php-dasar/pertemuan2/index.php
@@ -3,12 +3,60 @@
 // Sintaks PHP
 
 // Standar Output
-// echo, print
-// print_r
-// var_dump
+// echo, print 
+// print_r 
+// var_dump informasi type data dan ukuran
 
-echo "Galang Cahyo Murdoko";
-print "Galang Cahyo Murdoko";
-print_r ("Galang Cahyo Murdoko");
-var_dump ("Galang Cahyo Murdoko");
+// echo "Galang Cahyo Murdoko"; 
+// print "Galang Cahyo Murdoko"; 
+// print_r ("Galang Cahyo Murdoko"); 
+// var_dump ("Galang Cahyo Murdoko"); 
+//Penulisan syntax PHP
+// 1. PHP dalam HTML
+// 2. HTML dalam PHP
+ 
+//Variabel dan Tipe Data
+//Variabel
+//Nama tidak boleh diawali dengan angka
+//  $nama = "Galang";
+
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmetika
+// + -/ * %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+// penggabungstring
+// $nama_depan ="Galang";
+// $nama_belakang = "Murdoko";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 10;
+// $x -=5;
+// echo $x;
+// $nama = "Galang";
+// $nama .= " ";
+// $nama .= "Murdoko";
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identintas
+// ===. !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
 ?>
+
