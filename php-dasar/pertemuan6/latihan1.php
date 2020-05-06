ede4d18b16590d531811638fab020cd7307fc87c
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-07 05:57:16 +0700

    Mereview codingan pertemuan kemarin

---
+++ php-dasar/pertemuan6/latihan1.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <title>Latihan 1</title>
+    <style>
+        .kotak{
+            width: 50px; height: 50px; background-color: green;
+            text-align: center; line-height: 50px; margin: 3px;
+            float: left; transition: 1s;
+        }
+        .kotak:hover{
+            transform: rotate(360deg);
+            border-radius: 50%;
+        }
+        .clear{ clear: both;}
+    </style>
+</head>
+<body>
+    <?php
+        $angka = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ];
+    ?>
+
+    <?php foreach( $angka as $a) : ?>
+        <?php foreach( $a as $b) : ?>
+            <div class="kotak"> <?= $b; ?></div>
+        <?php endforeach; ?>
+        <div class="clear"></div>
+    <?php endforeach; ?>
+</body>
+</html>
