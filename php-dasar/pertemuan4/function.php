d941fd1f6aa151f6ca73d287d924806560beb7a3
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-06 16:00:03 +0700

    menyelesaikan function

---
+++ php-dasar/pertemuan4/function.php
@@ -0,0 +1,16 @@
+<?php
+function salam($waktu, $nama){
+    return "Selamat $waktu, $nama!";
+}
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?= echo salam("Siang","Galang"); ?></h1>
+</body>
+</html>
