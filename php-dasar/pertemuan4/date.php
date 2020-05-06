a806d61a582956a2c70a0ec491f8ea990cfe5872
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-06 15:50:03 +0700

    pertemuan 4 date done

---
+++ php-dasar/pertemuan4/date.php
@@ -0,0 +1,36 @@
+<?php
+//Date tanggal sesuai format
+// echo date("l, d-M-Y");
+
+//Time
+//UNIX timestamp
+// echo time();
+// echo date("l", time()+60*60*24*100);
+
+//mktime
+//mktime(0,0,0,0,0,0)
+//jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0,9,4,1996));
+
+//strtotime
+// echo date("l", strtotime("4 sep 1996"));
+
+//String
+
+//strlen() panjang string
+//strcmp() comparing string
+//explode() mengecek perhuruf string/ memecah string menjadi array
+//htmlspecialchars() untuk jaga jaga dari orang iseng 
+
+//Utility
+
+//var_dump() mencetak isi variable
+//isset() cek variable sudah dibikin / belum
+// empty() cek variabel yangg ada apakah kosong atau tidak
+// die() memberhentikan program
+// sleep() berhenti sementara
+
+//User-definied Function
+
+
+?>
