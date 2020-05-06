59319abbc4282eb79ef1c764ad696c519c7596e0
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-06 12:47:53 +0700

    menyelesaikan latihan 1 dan 2 pertemuan 3

---
+++ php-dasar/pertemuan3/latihan2.php
@@ -0,0 +1,19 @@
+<?php
+//Pengkondisian / Percabangan
+// if else
+// if else if else 
+// ternary
+// switch
+
+$x = 30;
+
+if ( $x < 10) {
+    echo "benar";
+} elseif($x== 30) {
+    echo "bingo!";
+}
+else {
+    echo "salah";
+}
+
+?>
