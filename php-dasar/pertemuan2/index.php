78272b0dd19585c00ba169eb8a05b1511d55449c
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-06 10:55:47 +0700

    menambahkan oertemuan 2

---
+++ php-dasar/pertemuan2/index.php
@@ -0,0 +1,14 @@
+<?php
+// Pertemuan 2 - PHP dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+echo "Galang Cahyo Murdoko";
+print "Galang Cahyo Murdoko";
+print_r ("Galang Cahyo Murdoko");
+var_dump ("Galang Cahyo Murdoko");
+?>
