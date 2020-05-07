231cb81f42fc82a9993ebf9ef15b220183721712
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-07 17:09:07 +0700

    membuat file latihan4.php, login.php, admin.php dan folder login

---
+++ php-dasar/pertemuan7/login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login admin</h1>
+    <ul>
+    <form action="" method="post">
+        <li>
+            <label for="username">username :</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">password :</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <button type="submit" name="submit">login</button>
+        </li>
+    
+    </form>
+    </ul>
+</body>
+</html>
