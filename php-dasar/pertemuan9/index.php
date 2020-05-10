73263f6ec7ef5e709440acec1af46a3efcc6033d
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-10 12:48:59 +0700

    git init

---
+++ php-dasar/pertemuan9/index.php
@@ -0,0 +1,38 @@
+<?php
+    mysqli_connect("localhost", "root", "", "phpdasar");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posters</title>
+</head>
+<body>
+    <h1>Poster</h1>
+    <table border= "1" cellpadding= "10" cellspacing= "0">
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>Judul</th>
+        <th>Harga</th>
+        <th>Artist</th>
+        <th>Email</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>
+            <a href="">ubah</a>
+            <a href="">hapus</a>
+        </td>
+        <td> <img src="" alt=""></td>
+        <td>judul</td>
+        <td>5000</td>
+        <td>@murdoko_</td>
+        <td>mail@mail.com</td>
+    </tr>
+    </table>
+</body>
+</html>
