60aa80c88b55f48e791e56857a2f86dca70bb507
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-10 14:13:01 +0700

    membuat file tambah.php

---
+++ php-dasar/pertemuan10/tambah.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data poster</title>
+</head>
+<body>
+<h1>Tambah data poster</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="title">Title: </label>
+                <input type="text" name="title" id="title">
+            </li>
+            <li>
+                <label for="artist">Artist: </label>
+                <input type="text" name="artist" id="artist">
+            </li>
+            <li>
+                <label for="price">Price: IDR </label>
+                <input type="text" name="price" id="price">
+            </li>
+            <li>
+                <label for="email">Email: </label>
+                <input type="email" name="email" id="email">
+            </li>
+            
+        </ul>
+    </form>
+</body>
+</html>
