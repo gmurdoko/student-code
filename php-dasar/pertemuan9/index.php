2601e0c351b88d075734ce9b16be893463082411
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-10 13:51:06 +0700

    membuat function.php dan cadangan index2.php

--- php-dasar/pertemuan9/index.php
+++ php-dasar/pertemuan9/index.php
@@ -1,18 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "phpdasar");
-
-    $result = mysqli_query($conn, "SELECT * FROM posters");
-    // var_dump($result);
-
-// ambil data fetch posters daro object result
-// mysqli_fetch_row() array numerik
-// mysqli_fetch_assoc() array asosiatif
-// mysqli_fetch_array() array asosiatif fan numerik
-// mysqli_fetch_object() 
-
-    // while($poster = mysqli_fetch_assoc($result)) {
-    //     var_dump($poster);
-    // }
+require 'functions.php';
+$posters = query("SELECT * FROM posters");
 ?>
 
 <!DOCTYPE html>
@@ -34,20 +22,25 @@
         <th>Artist</th>
         <th>Email</th>
     </tr>
-    <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
+    <?php 
+        $i = 1;
+        foreach ( $posters as $poster ) : ?>
     <tr>
-        <td>1</td>
+        <td><?= $i ?></td>
         <td>
             <a href="">ubah</a> | 
             <a href="">hapus</a>
         </td>
-        <td> <img src="img/<?= $row["images"]; ?>" width="50"></td>
-        <td><?= $row["title"];?></td>
-        <td><?= $row["price"];?></td>
-        <td><?= $row["artist"];?></td>
-        <td><?= $row["email"];?></td>
+        <td> <img src="img/<?= $poster["images"]; ?>" width="50"></td>
+        <td><?= $poster["title"];?></td>
+        <td><?= $poster["price"];?></td>
+        <td><?= $poster["artist"];?></td>
+        <td><?= $poster["email"];?></td>
     </tr>
-    <?php endwhile; ?>
+    <?php 
+    $i++;
+    endforeach;
+    ?>
     </table>
 </body>
 </html>
