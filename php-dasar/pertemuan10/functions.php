0d6659e0d484bc29661632ea768285e285c36e29
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-10 16:15:00 +0700

    menambahkan fitur alarm menggunakan js dan htmlspecialchars di function.php dan require di tambah.php

--- php-dasar/pertemuan10/functions.php
+++ php-dasar/pertemuan10/functions.php
@@ -11,4 +11,23 @@ function query($query){
     return $rows;
 }
 
+function tambah($data){
+    global $conn;
+
+    $title = htmlspecialchars($data["title"]);
+    $artist = htmlspecialchars($data["artist"]);
+    $price = htmlspecialchars($data["price"]);
+    $email = htmlspecialchars($data["email"]);
+    $images = htmlspecialchars($data["images"]);
+    
+    $query = "INSERT INTO posters
+            VALUES 
+            ('','$title', '$artist', '$price', '$email', '$images')
+            ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+
+}
+
 ?>
--- php-dasar/pertemuan10/tambah.php
+++ php-dasar/pertemuan10/tambah.php
@@ -1,20 +1,23 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "phpdasar");
-
+require 'functions.php';
 
 if ( isset($_POST["submit"]) ) {
-    $title = $_POST["title"];
-    $artist = $_POST["artist"];
-    $price = $_POST["price"];
-    $email = $_POST["email"];
-    $images = $_POST["images"];
-    
-    
-    $query = "INSERT INTO posters
-            VALUES 
-            ('','$title', '$artist', '$price', '$email', '$images')
-            ";
-    mysqli_query($conn, $query);
+
+    if ( tambah($_POST) > 0) {
+        echo "
+            <script>
+                alert('data berhasil ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+                alert('data gagal ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
 
 }
 
@@ -33,23 +36,23 @@ if ( isset($_POST["submit"]) ) {
         <ul>
             <li>
                 <label for="title">Title: </label>
-                <input type="text" name="title" id="title">
+                <input type="text" name="title" id="title" required>
             </li>
             <li>
                 <label for="artist">Artist: </label>
-                <input type="text" name="artist" id="artist">
+                <input type="text" name="artist" id="artist" required>
             </li>
             <li>
                 <label for="price">Price: IDR </label>
-                <input type="text" name="price" id="price">
+                <input type="text" name="price" id="price" required>
             </li>
             <li>
                 <label for="email">Email: </label>
-                <input type="text" name="email" id="email">
+                <input type="text" name="email" id="email" required>
             </li>
             <li>
                 <label for="images">Images: </label>
-                <input type="text" name="images" id="images">
+                <input type="text" name="images" id="images" required>
             </li>
             <li>
                 <button type="submit" name="submit">Tambah data</button>
