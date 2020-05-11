e4f4d207674739f634a6518ca2b5c92b07b7a3a5
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-11 17:44:09 +0700

    menambakan laman ubah

--- php-dasar/pertemuan11/ubah.php
+++ php-dasar/pertemuan11/ubah.php
@@ -1,19 +1,24 @@
 <?php
 require 'functions.php';
 
+$id = $_GET["id"];
+
+$poster = query("SELECT * FROM posters WHERE id = $id")[0];
+
+
 if ( isset($_POST["submit"]) ) {
 
-    if ( tambah($_POST) > 0) {
+    if ( ubah($_POST) > 0) {
         echo "
             <script>
-                alert('data berhasil ditambahkan!');
+                alert('data berhasil diubah!');
                 document.location.href = 'index.php';
             </script>
         ";
     } else {
         echo "
             <script>
-                alert('data gagal ditambahkan!');
+                alert('data gagal diubah!');
                 document.location.href = 'index.php';
             </script>
         ";
@@ -28,34 +33,34 @@ if ( isset($_POST["submit"]) ) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah data poster</title>
+    <title>Ubah data poster</title>
 </head>
 <body>
-<h1>Tambah data poster</h1>
+<h1>Ubah data poster</h1>
     <form action="" method="post">
         <ul>
             <li>
                 <label for="title">Title: </label>
-                <input type="text" name="title" id="title" required>
+                <input type="text" name="title" id="title" required value="<?= $poster["title"]; ?>">
             </li>
             <li>
                 <label for="artist">Artist: </label>
-                <input type="text" name="artist" id="artist" required>
+                <input type="text" name="artist" id="artist" required value="<?= $poster["artist"]; ?>">
             </li>
             <li>
                 <label for="price">Price: IDR </label>
-                <input type="text" name="price" id="price" required>
+                <input type="text" name="price" id="price" required value="<?= $poster["price"]; ?>">
             </li>
             <li>
                 <label for="email">Email: </label>
-                <input type="text" name="email" id="email" required>
+                <input type="text" name="email" id="email" required value="<?= $poster["email"]; ?>">
             </li>
             <li>
                 <label for="images">Images: </label>
-                <input type="text" name="images" id="images" required>
+                <input type="text" name="images" id="images" required value="<?= $poster["images"]; ?>">
             </li>
             <li>
-                <button type="submit" name="submit">Tambah data</button>
+                <button type="submit" name="submit">Ubah data</button>
             </li>
             
         </ul>
