0be3432a933f471682c3f453709af38a4aadf00b
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-19 19:23:22 +0700

    menambahkan file registrasi.php

---
+++ php-dasar/pertemuan14/registrasi.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Registrasi</title>
+</head>
+<body>
+
+<h1>Halaman Registrasi</h1>
+
+<form action="" method="post">
+
+    <ul>
+        <li>
+            <label for="username">username :</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">password :</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <label for="password2">konfirmasi password :</label>
+            <input type="password" name="password2" id="password2">
+        </li>
+        <li>
+            <label for="email">email :</label>
+            <input type="email" name="email" id="email">
+        </li>
+    
+    </ul>
+
+</form>
+    
+</body>
+</html>
