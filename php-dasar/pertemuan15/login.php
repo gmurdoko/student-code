61d3bb22d82380c6f9d499cef97c10f1a8eff359
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-19 20:18:15 +0700

    membua file login.php

---
+++ php-dasar/pertemuan15/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <ul>
+        <li>
+            <label for="username">username  : </label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">password  : </label>
+            <input type="password" name="password" id="password">
+        </li>
+    </ul>
+</body>
+</html>
