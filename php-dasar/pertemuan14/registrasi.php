d620755e22925123b9959e36ddfacfa9f79eb5c1
gmurdoko <galangcahyomurdoko@gmail.com>
2020-05-19 20:06:14 +0700

    menambahkan fitur registrasi admin

--- php-dasar/pertemuan14/registrasi.php
+++ php-dasar/pertemuan14/registrasi.php
@@ -1,16 +1,36 @@
+<?php
+
+require 'functions.php';
+
+if (isset($_POST["register"])) {
+    
+    if (registrasi($_POST) > 0) {
+        echo "<script>
+            alert('user baru berhasil ditambahkan!');
+        </script>";
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Registrasi</title>
+    <style>
+        label {
+            display: block;
+        }
+    </style>
 </head>
 <body>
 
 <h1>Halaman Registrasi</h1>
 
 <form action="" method="post">
-
     <ul>
         <li>
             <label for="username">username :</label>
@@ -25,12 +45,9 @@
             <input type="password" name="password2" id="password2">
         </li>
         <li>
-            <label for="email">email :</label>
-            <input type="email" name="email" id="email">
+            <button type="submit" name="register">sign up</button>
         </li>
-    
     </ul>
-
 </form>
     
 </body>
